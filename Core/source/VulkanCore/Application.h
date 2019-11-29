30848ceb4d0a9a382637cd63dd425458670e67b8
Rink Wilbrink <rinkwilbrink1@gmail.com>
2019-11-29 14:50:53 +0100

    copyBuffer and createVertexBuffer reworked.

--- Core/source/VulkanCore/Application.h
+++ Core/source/VulkanCore/Application.h
@@ -194,6 +194,10 @@ private:
 
 	void createVertexBuffer();
 
+	void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
+
+	void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
+
 	uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
 
 	void createCommandBuffers();
